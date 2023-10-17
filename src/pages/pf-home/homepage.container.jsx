f57e45456f8b4d87d5f08d9467faa6b400358d0f
AryanVik <99641342+AryanVik@users.noreply.github.com>
2023-10-18 02:02:30 +0530

    work in progress

--- src/pages/pf-home/homepage.container.jsx
+++ src/pages/pf-home/homepage.container.jsx
@@ -1,10 +1,13 @@
 import Navbar from "../../component/Navbar/navbar";
+import LoginPage from "../pf-login";
+import LoginPage2 from "../pf-login/login";
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
-      <h1>Homepage</h1>
+      {/* <LoginPage/> */}
+      <LoginPage2/>
     </>
   );
 };
