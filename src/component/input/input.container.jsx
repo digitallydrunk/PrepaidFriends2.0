10c6b029c58568df738edfc50deb06c728e15975
Vedansh Mehra <vedanshofficial@gmail.com>
2023-10-19 02:42:27 +0530

    code formatting and spelling correction

--- src/component/input/input.container.jsx
+++ src/component/input/input.container.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styles from "./input.container.module.css";
-import { FiEye,FiEyeOff } from "react-icons/fi";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+
 const PFInput = ({
   name,
   id,
@@ -13,10 +14,8 @@ const PFInput = ({
   onChange,
   ...props
 }) => {
-
-  const[hidden,setHidden] = useState(true)
+  const [hidden, setHidden] = useState(true);
   return (
-    
     <>
       <label className="font-semibold" htmlFor={htmlFor}>
         {`${label}`}:
@@ -26,14 +25,19 @@ const PFInput = ({
           <input
             name={name}
             id={id}
-            type={hidden?'password':'text'}
+            type={hidden ? "password" : "text"}
             className="form-input mt-3 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
             placeholder={placeholder}
             value={value}
             onChange={onChange}
             {...props}
           />
-          <button onClick={()=>setHidden(!hidden)} className={styles.insidePostion}>{hidden?<FiEye/>:<FiEyeOff/>}</button>
+          <button
+            onClick={() => setHidden(!hidden)}
+            className={styles.insidePosition}
+          >
+            {hidden ? <FiEye /> : <FiEyeOff />}
+          </button>
         </div>
       ) : (
         <div className="relative">
@@ -47,12 +51,13 @@ const PFInput = ({
             onChange={onChange}
             {...props}
           />
-          {addOnAfter && <div className={styles.insidePostion}>{addOnAfter}</div> }
+          {addOnAfter && (
+            <div className={styles.insidePosition}>{addOnAfter}</div>
+          )}
         </div>
       )}
     </>
   );
 };
-
 
 export { PFInput };
