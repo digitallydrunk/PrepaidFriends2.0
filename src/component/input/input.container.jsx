67ed10d23bffdfa06e529500fe30cc1183a5a944
AryanVik <99641342+AryanVik@users.noreply.github.com>
2023-10-19 02:06:42 +0530

    Updated the input component

--- src/component/input/input.container.jsx
+++ src/component/input/input.container.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+import styles from "./input.container.module.css";
+import { FiEye,FiEyeOff } from "react-icons/fi";
 const PFInput = ({
   name,
   id,
   label,
+  addOnAfter,
   htmlFor,
   type = "input",
   placeholder,
@@ -9,23 +13,46 @@ const PFInput = ({
   onChange,
   ...props
 }) => {
+
+  const[hidden,setHidden] = useState(true)
   return (
+    
     <>
       <label className="font-semibold" htmlFor={htmlFor}>
         {`${label}`}:
       </label>
-      <input
-        name={name}
-        id={id}
-        type={type}
-        className="form-input mt-3 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        {...props}
-      />
+      {type === "password" ? (
+        <div className="relative">
+          <input
+            name={name}
+            id={id}
+            type={hidden?'password':'text'}
+            className="form-input mt-3 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            {...props}
+          />
+          <button onClick={()=>setHidden(!hidden)} className={styles.insidePostion}>{hidden?<FiEye/>:<FiEyeOff/>}</button>
+        </div>
+      ) : (
+        <div className="relative">
+          <input
+            name={name}
+            id={id}
+            type={type}
+            className="form-input mt-3 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            {...props}
+          />
+          {addOnAfter && <div className={styles.insidePostion}>{addOnAfter}</div> }
+        </div>
+      )}
     </>
   );
 };
+
 
 export { PFInput };
