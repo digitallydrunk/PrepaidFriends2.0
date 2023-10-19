b184b30e21054040c45acf2e4e1a8608c7b8676c
rituraj singh <riturajsingh5454@gmail.com>
2023-10-20 01:40:36 +0530

    created global table component

--- src/pages/sample/index.jsx
+++ src/pages/sample/index.jsx
@@ -1,15 +1,17 @@
 import PFInput from "../../component/input";
+import Tablecomp from "../../component/Table";
 
 const Sample = () => {
   return (
-    <PFInput
-      name={"email"}
-      id={"email"}
-      type="email"
-      label={"Email"}
-      placeholder={"Enter email address..."}
-      htmlFor={"email"}
-    />
+    // <PFInput
+    //   name={"email"}
+    //   id={"email"}
+    //   type="email"
+    //   label={"Email"}
+    //   placeholder={"Enter email address..."}
+    //   htmlFor={"email"}
+    // />
+    <Tablecomp/>
   );
 };
 
