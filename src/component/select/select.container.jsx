7edd6907c675d8f2719d3bc752bc100f566f0c49
AryanVik <99641342+AryanVik@users.noreply.github.com>
2023-10-21 02:53:55 +0530

    fixed

--- src/component/select/select.container.jsx
+++ src/component/select/select.container.jsx
@@ -1,26 +1,26 @@
-const PFSelect = ({label,options,onChange,value,...props}) => {
-    const handleChange = (event) => {
-        const selectedValue = event.target.value;
-        onChange(selectedValue);
-    };
+const PFSelect = ({ label, options, onChange, value, ...props }) => {
+  const handleChange = (event) => {
+    const selectedValue = event.target.value;
+    onChange(selectedValue);
+  };
   return (
     <>
       <div className="lg:col-span-4">
-      <label className="font-semibold">{label}:</label>
-      <select
-        className="form-select form-input mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
-        value={value} 
-        onChange={handleChange} 
-        {...props}
-      >
-        {options?.map((option, index) => (
-          <option key={index} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
-    </div>
+        <label className="font-semibold">{label}</label>
+        <select
+          className="form-select form-input mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
+          value={value}
+          onChange={handleChange}
+          {...props}
+        >
+          {options?.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
     </>
   );
 };
-export {PFSelect}
+export { PFSelect };
--- src/pages/sample/index.jsx
+++ src/pages/sample/index.jsx
@@ -1,21 +1,25 @@
+import { PFInput } from "../../component/input/input.container";
 import PFSelect from "../../component/select";
 import { useState } from "react";
 
 const Sample = () => {
-  const [selectedOption, setSelectedOption] = useState('IT');
+  const [selectedOption, setSelectedOption] = useState("IT");
   const Options = [
-    { value: 'IT', label: 'Information Technology' },
-    { value: 'CSE', label: 'Computer Science' },
-    { value: 'AI', label: 'Artificail Intelligence' },
+    { value: "IT", label: "Information Technology" },
+    { value: "CSE", label: "Computer Science" },
+    { value: "AI", label: "Artificail Intelligence" },
   ];
-  
+
   return (
-    <PFSelect
+    <>
+      <PFInput label="Email"/>
+      <PFSelect
         label="Choose Your Branch"
         options={Options}
         value={selectedOption}
         onChange={setSelectedOption}
       />
+    </>
   );
 };
 
