4f4eb8f1246fb6bdb6e87961a3bba6dfe6c1ae25
Atoleanantha <103057343+Atoleanantha@users.noreply.github.com>
2023-10-20 22:45:58 +0530

    pf-tag commit

---
+++ src/pf-tag/container.jsx
@@ -0,0 +1,102 @@
+// pf-tag/container.jsx
+import React from 'react';
+
+const TagContainer = () => {
+  return (
+    <div>
+      {/* Primary Variant */}
+      <div className="tag tag-primary">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="feather feather-bell-off mr-2"
+        >
+          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+          <path d="M18.63 13A17.89 17.89 0 0 1 18 8"></path>
+          <path d="M6.26 6.26A5.86 5.86 0 0 0 6 8c0 7-3 9-3 9h14"></path>
+          <path d="M18 8a6 6 0 0 0-9.33-5"></path>
+          <line x1="1" y1="1" x2="23" y2="23"></line>
+        </svg>
+        Tag (Primary)
+      </div>
+
+      {/* Success Variant */}
+      <div className="tag tag-success">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="green"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="feather feather-bell-off mr-2"
+        >
+          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+          <path d="M18.63 13A17.89 17.89 0 0 1 18 8"></path>
+          <path d="M6.26 6.26A5.86 5.86 0 0 0 6 8c0 7-3 9-3 9h14"></path>
+          <path d="M18 8a6 6 0 0 0-9.33-5"></path>
+          <line x1="1" y1="1" x2="23" y2="23"></line>
+        </svg>
+        Tag (Success)
+      </div>
+
+      {/* Error Variant */}
+      <div className="tag tag-error">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="red"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="feather feather-bell-off mr-2"
+        >
+          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+          <path d="M18.63 13A17.89 17.89 0 0 1 18 8"></path>
+          <path d="M6.26 6.26A5.86 5.86 0 0 0 6 8c0 7-3 9-3 9h14"></path>
+          <path d="M18 8a6 6 0 0 0-9.33-5"></path>
+          <line x1="1" y1="1" x2="23" y2="23"></line>
+        </svg>
+        Tag (Error)
+      </div>
+
+      {/* Warning Variant */}
+      <div className="tag tag-warning">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="orange"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="feather feather-bell-off mr-2"
+        >
+          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+          <path d="M18.63 13A17.89 17.89 0 0 1 18 8"></path>
+          <path d="M6.26 6.26A5.86 5.86 0 0 0 6 8c0 7-3 9-3 9h14"></path>
+          <path d="M18 8a6 6 0 0 0-9.33-5"></path>
+          <line x1="1" y1="1" x2="23" y2="23"></line>
+        </svg>
+        Tag (Warning)
+      </div>
+    </div>
+  );
+};
+
+export default TagContainer;
---
+++ src/sample.js
@@ -0,0 +1,14 @@
+// src/App.js (or any other React component file in your project)
+import React from 'react';
+import TagContainer from './pf-tag/container';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <TagContainer /> {/* Render the TagContainer component */}
+    </div>
+  );
+}
+
+export default App;
