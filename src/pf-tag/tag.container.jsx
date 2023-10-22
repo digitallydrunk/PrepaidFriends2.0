ea7a6c2641ea80f017b2d4ae50761bbecab7b2db
Atoleanantha <103057343+Atoleanantha@users.noreply.github.com>
2023-10-22 12:53:11 +0530

    tag cpmponent created

--- src/pf-tag/tag.container.jsx
+++ src/pf-tag/tag.container.jsx
@@ -1,102 +1,59 @@
-// pf-tag/container.jsx
 import React from 'react';
 
-const TagContainer = () => {
-  return (
-    <div>
-      {/* Primary Variant */}
-      <div className="tag tag-primary">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className="feather feather-bell-off mr-2"
-        >
-          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-          <path d="M18.63 13A17.89 17.89 0 0 1 18 8"></path>
-          <path d="M6.26 6.26A5.86 5.86 0 0 0 6 8c0 7-3 9-3 9h14"></path>
-          <path d="M18 8a6 6 0 0 0-9.33-5"></path>
-          <line x1="1" y1="1" x2="23" y2="23"></line>
-        </svg>
-        Tag (Primary)
-      </div>
-
-      {/* Success Variant */}
-      <div className="tag tag-success">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="green"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className="feather feather-bell-off mr-2"
-        >
-          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-          <path d="M18.63 13A17.89 17.89 0 0 1 18 8"></path>
-          <path d="M6.26 6.26A5.86 5.86 0 0 0 6 8c0 7-3 9-3 9h14"></path>
-          <path d="M18 8a6 6 0 0 0-9.33-5"></path>
-          <line x1="1" y1="1" x2="23" y2="23"></line>
-        </svg>
-        Tag (Success)
-      </div>
-
-      {/* Error Variant */}
-      <div className="tag tag-error">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="red"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className="feather feather-bell-off mr-2"
-        >
-          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-          <path d="M18.63 13A17.89 17.89 0 0 1 18 8"></path>
-          <path d="M6.26 6.26A5.86 5.86 0 0 0 6 8c0 7-3 9-3 9h14"></path>
-          <path d="M18 8a6 6 0 0 0-9.33-5"></path>
-          <line x1="1" y1="1" x2="23" y2="23"></line>
-        </svg>
-        Tag (Error)
-      </div>
-
-      {/* Warning Variant */}
-      <div className="tag tag-warning">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="orange"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          className="feather feather-bell-off mr-2"
-        >
-          <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-          <path d="M18.63 13A17.89 17.89 0 0 1 18 8"></path>
-          <path d="M6.26 6.26A5.86 5.86 0 0 0 6 8c0 7-3 9-3 9h14"></path>
-          <path d="M18 8a6 6 0 0 0-9.33-5"></path>
-          <line x1="1" y1="1" x2="23" y2="23"></line>
-        </svg>
-        Tag (Warning)
-      </div>
-    </div>
-  );
+const tagConfigurations = {
+  Primary: {
+    bgColor: 'bg-blue-200',
+    textColor: 'text-blue-700',
+    icon: 'feather-bell-off',
+  },
+  Success: {
+    bgColor: 'bg-green-200',
+    textColor: 'text-green-700',
+    icon: 'feather-arrow-right',
+  },
+  Warning: {
+    bgColor: 'bg-orange-200',
+    textColor: 'text-orange-700',
+    icon: 'feather-activity',
+  },
+  Error: {
+    bgColor: 'bg-red-200',
+    textColor: 'text-red-700',
+    icon: 'feather-archive',
+  },
+  Default: {
+    bgColor: 'bg-white',
+    textColor: 'text-gray-700',
+    icon: 'feather-hard-drive',
+  },
 };
 
-export default TagContainer;
+function DynamicTags({ variant = 'Default', label }) {
+  const tagConfiguration = tagConfigurations[variant];
+
+  if (!tagConfiguration) {
+    return null;
+  }
+
+  const { bgColor, textColor, icon } = tagConfiguration;
+
+  return (
+    <div className={`text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full ${bgColor} ${textColor} border`}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className={`feather ${icon} mr-2`}
+      />
+      {label || variant}
+    </div>
+  );
+}
+
+export default DynamicTags;
