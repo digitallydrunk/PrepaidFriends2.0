b9c9b835389930f29e029625eb725a6f8d963d6a
AryanVik <99641342+AryanVik@users.noreply.github.com>
2023-10-24 00:46:02 +0530

    minor updates

--- src/component/accordion/accordion.container.jsx
+++ src/component/accordion/accordion.container.jsx
@@ -1,23 +1,45 @@
 import React, { useState } from "react";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
+import styles from './accordion.module.css';
 
 const PFAccordion = ({ items }) => {
-  const [activeIndex, setActiveIndex] = useState(null);
+  const [activeIndexes, setActiveIndexes] = useState([]);
 
   const handleClick = (index) => {
-    setActiveIndex(index === activeIndex ? null : index);
+    if (activeIndexes.includes(index)) {
+      setActiveIndexes(activeIndexes.filter((item) => item !== index));
+    } else {
+      setActiveIndexes([...activeIndexes, index]);
+    }
   };
+
   return (
     <div>
       {items?.map((item, index) => (
-        <div key={index}>
-          <div className="flex border-b border-b-black">
-            <p className="font-bold">{item.title}</p>
-            <button onClick={() => handleClick(index)}>
-              {activeIndex === index ? "Hide" : "Show"}
-            </button>
-          </div>
+        <div key={item.content}>
+          <div className="border-b border-black py-4">
+            <div
+              onClick={() => handleClick(index)}
+              className="flex justify-between cursor-pointer mb-1 p-1"
+            >
+              <h3 className="font-bold">{item.title}</h3>
+              <button>
+                {activeIndexes.includes(index) ? (
+                  <FiChevronUp className={`${styles.uparrow} ${styles.bold}`} />
+                ) : (
+                  <FiChevronDown className={styles.bold} />
+                )}
+              </button>
+            </div>
 
-          {activeIndex === index && <div>{item.content}</div>}
+            <div
+              className={`${styles.content} ${
+                activeIndexes.includes(index) ? styles.expanded : ""
+              }`}
+            >
+              {item.content}
+            </div>
+          </div>
         </div>
       ))}
     </div>
