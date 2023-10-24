abb38bac5a25dc1675a4ca4e083e73a0d1bc8df8
iamavs09 <aryanvikas009@gmail.com>
2023-10-25 03:33:02 +0530

    completed accordion animation

--- src/component/accordion/accordion.container.jsx
+++ src/component/accordion/accordion.container.jsx
@@ -16,33 +16,3 @@ const PFAccordion = ({ data }) => {
 };
 
 export { PFAccordion };
-
-{/* <div>
-      {items?.map((item, index) => (
-        <div key={item.content}>
-          <div className="border-b border-black py-4">
-            <div
-              onClick={() => handleClick(index)}
-              className="flex justify-between cursor-pointer mb-1 p-1"
-            >
-              <h3 className="font-bold">{item.title}</h3>
-              <button>
-                {activeIndexes.includes(index) ? (
-                  <FiChevronUp className={`${styles.uparrow} ${styles.bold}`} />
-                ) : (
-                  <FiChevronDown className={styles.bold} />
-                )}
-              </button>
-            </div>
-
-            <div
-              className={`${styles.content} ${
-                activeIndexes.includes(index) ? styles.expanded : ""
-              }`}
-            >
-              {item.content}
-            </div>
-          </div>
-        </div>
-      ))}
-    </div> */}
