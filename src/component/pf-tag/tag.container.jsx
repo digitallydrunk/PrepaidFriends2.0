1b5f2b658459ce9fe2c445d7c94379bb3a93b238
Atoleanantha <103057343+Atoleanantha@users.noreply.github.com>
2023-10-25 02:59:01 +0530

    tags created

--- src/component/pf-tag/tag.container.jsx
+++ src/component/pf-tag/tag.container.jsx
@@ -1,62 +1,71 @@
 import React from 'react';
+import style from './tag.containe.module.css';
 
-const tagConfigurations = {
-  Primary: {
-    bgColor: 'bg-blue-200',
-    textColor: 'text-blue-700',
-    icon: 'feather-bell-off',
-  },
-  Success: {
-    bgColor: 'bg-green-200',
-    textColor: 'text-green-700',
-    icon: 'feather-arrow-right',
-  },
-  Warning: {
-    bgColor: 'bg-orange-200',
-    textColor: 'text-orange-700',
-    icon: 'feather-activity',
-  },
-  Error: {
-    bgColor: 'bg-red-200',
-    textColor: 'text-red-700',
-    icon: 'feather-archive',
-  },
-  Default: {
-    bgColor: 'bg-white',
-    textColor: 'text-gray-700',
-    icon: 'feather-hard-drive',
-  },
-};
+function PFTag({ variant, label, onClick }) {
+  let tagContent = null;
 
-function PFTag({ variant = 'Default', label, onClick }) {
-  const tagConfiguration = tagConfigurations[variant];
+  switch (variant) {
+    case 'Primary':
+      tagContent = (
+        <div
+          style={{ marginLeft: 0 }}
+          className={`text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full ${style.primary_bg} ${style.primary_text} border`}
+          onClick={onClick}
+        >
+          {label || variant}
+        </div>
+      );
+      break;
 
-  if (!tagConfiguration) {
-    return null;
+    case 'Success':
+      tagContent = (
+        <div
+          style={{ marginLeft: 0 }}
+          className={`text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full ${style.success_bg} ${style.success_text} border`}
+          onClick={onClick}
+        >
+          {label || variant}
+        </div>
+      );
+      break;
+
+    case 'Warning':
+      tagContent = (
+        <div
+          style={{ marginLeft: 0 }}
+          className={`text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full ${style.warning_bg} ${style.warning_text} border`}
+          onClick={onClick}
+        >
+          {label || variant}
+        </div>
+      );
+      break;
+
+    case 'Error':
+      tagContent = (
+        <div
+          style={{ marginLeft: 0 }}
+          className={`text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full ${style.error_bg} ${style.error_text} border`}
+          onClick={onClick}
+        >
+          {label || variant}
+        </div>
+      );
+      break;
+
+    default:
+      tagContent = (
+        <div
+          style={{ marginLeft: 0 }}
+          className={`text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full ${style.default_bg} ${style.default_text} border`}
+          onClick={onClick}
+        >
+          {label || variant}
+        </div>
+      );
   }
 
-  const { bgColor, textColor, icon } = tagConfiguration;
-
-  return (
-    <div
-      className={`text-xs inline-flex items-center font-bold leading-sm uppercase px-3 py-1 rounded-full ${bgColor} ${textColor} border`}
-      onClick={onClick}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        className={`feather ${icon} mr-2`}
-      />
-      {label || variant}
-    </div>
-  );
+  return tagContent;
 }
 
-export  {PFTag};
+export { PFTag };
--- src/pages/sample/index.jsx
+++ src/pages/sample/index.jsx
@@ -1,10 +1,15 @@
 import PFCheckbox from "../../component/checkbox";
 import PFInput from "../../component/input";
 import LoginPage from "../pf-login";
-
+import PFTag from "../../component/pf-tag";
 const Sample = () => {
   return (
     <>
+      <PFTag variant="Primary" label="Primary Tag" />
+      <PFTag variant="Success" label="Success Tag" />
+      <PFTag variant="Warning" label="Warning Tag" />
+      <PFTag variant="Error" label="Error Tag" />
+      <PFTag label="Default Tag" />
       <PFInput
         name={"email"}
         id={"email"}
