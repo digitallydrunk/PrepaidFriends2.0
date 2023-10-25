188cde17b9dd57c130ed00bc579c32fa67a270ca
Md.Mohiuddin <saddammd203@gmail.com>
2023-10-26 02:41:28 +0600

    resolved

--- src/pages/sample/index.jsx
+++ src/pages/sample/index.jsx
@@ -3,7 +3,6 @@ import PFCheckbox from "../../component/checkbox";
 import PFInput from "../../component/input";
 import { FeaturesCard } from "../../component/features-card/features-card.container";
 import LoginPage from "../pf-login";
-import { Button } from "../../component/button/button.container";
 
 const Sample = () => {
   const [selectedOption, setSelectedOption] = useState("IT");
@@ -15,7 +14,6 @@ const Sample = () => {
 
   return (
     <>
-       <Button></Button>
       <FeaturesCard
         imageSrc="https://templatekit.tokomoo.com/paymentkit2/wp-content/uploads/sites/112/2022/11/feature-1.png"
         title="Grow Your Business"
