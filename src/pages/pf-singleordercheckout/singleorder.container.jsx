59aca592333bafd773383e8ccdac5e1367621b2c
rituraj singh <riturajsingh5454@gmail.com>
2023-10-26 03:13:35 +0530

    create outline for component

--- src/pages/pf-singleordercheckout/singleorder.container.jsx
+++ src/pages/pf-singleordercheckout/singleorder.container.jsx
@@ -39,11 +39,11 @@ function SingleOrderCheckout() {
                   <div>
                     <div className="lg:col-span-6">
                       <label className="form-label font-semibold">
-                        Your Email : <span className="text-red-600">*</span>
+                         Email Address: <span className="text-red-600">*</span>
                       </label>
                       <input
                         type="email"
-                        className="form-input w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0 mt-2"
+                        className="form-input py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0 mt-2"
                         placeholder="Email"
                         name="email"
                         required=""
@@ -52,139 +52,33 @@ function SingleOrderCheckout() {
 
                     <div className="lg:col-span-4">
                       <label className="font-semibold">Payement Mode:</label>
-                      <select className="form-select form-input mt-2 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0">
+                      <select className="form-select form-input mt-2 py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0">
                         <option value="BITCOIN">Bitcoin</option>
                         <option value="CARD">Card</option>
                         <option value="VISA">Visa</option>
                       </select>
                     </div>
+                    <div className="lg:col-span-6">
+                      <label className="form-label font-semibold">
+                        Invice ID : <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        type="text"
+                        className="form-input  py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0 mt-2"
+                        placeholder="Envice id"
+                        name="Envice"
+                        required=""
+                      />
+                    </div>
                   </div>
                 </form>
 
                 <h3 className="text-xl leading-normal font-semibold mt-6">
-                  Payment
+                  Prepaid card exchange fee 1*$1.98
                 </h3>
-                <form>
-                  <div className="grid lg:grid-cols-12 grid-cols-1 mt-6 gap-5">
-                    <div className="lg:col-span-12">
-                      <div className="block">
-                        <div>
-                          <label className="inline-flex items-center">
-                            <input
-                              type="radio"
-                              readOnly
-                              className="form-radio border-gray-200 dark:border-gray-800 text-indigo-600 focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50 me-2"
-                              name="radio-colors"
-                              value="1"
-                              checked
-                            />
-                            <span className="text-slate-400">Credit card</span>
-                          </label>
-                        </div>
-                      </div>
-
-                      <div className="block mt-2">
-                        <div>
-                          <label className="inline-flex items-center">
-                            <input
-                              type="radio"
-                              readOnly
-                              className="form-radio border-gray-200 dark:border-gray-800 text-indigo-600 focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50 me-2"
-                              name="radio-colors"
-                              value="1"
-                            />
-                            <span className="text-slate-400">Debit Card</span>
-                          </label>
-                        </div>
-                      </div>
-
-                      <div className="block mt-2">
-                        <div>
-                          <label className="inline-flex items-center">
-                            <input
-                              type="radio"
-                              readOnly
-                              className="form-radio border-gray-200 dark:border-gray-800 text-indigo-600 focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50 me-2"
-                              name="radio-colors"
-                              value="1"
-                            />
-                            <span className="text-slate-400">PayPal</span>
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="lg:col-span-6">
-                      <label className="form-label font-semibold">
-                        Account Holder Name :{" "}
-                        <span className="text-red-600">*</span>
-                      </label>
-                      <input
-                        type="text"
-                        readOnly
-                        className="form-input w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0 mt-2"
-                        placeholder="Name:"
-                        id="accountname"
-                        name="name"
-                        required=""
-                      />
-                    </div>
-
-                    <div className="lg:col-span-6">
-                      <label className="form-label font-semibold">
-                        Credit card number :{" "}
-                        <span className="text-red-600">*</span>
-                      </label>
-                      <input
-                        type="number"
-                        readOnly
-                        className="form-input w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0 mt-2"
-                        placeholder="**** **** **** ****"
-                        id="cardnumber"
-                        name="number"
-                        required=""
-                      />
-                    </div>
-
-                    <div className="lg:col-span-3">
-                      <label className="form-label font-semibold">
-                        Expiration : <span className="text-red-600">*</span>
-                      </label>
-                      <input
-                        type="number"
-                        readOnly
-                        className="form-input w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0 mt-2"
-                        placeholder=""
-                        id="expiration"
-                        name="number"
-                        required=""
-                      />
-                    </div>
-
-                    <div className="lg:col-span-3">
-                      <label className="form-label font-semibold">
-                        CVV : <span className="text-red-600">*</span>
-                      </label>
-                      <input
-                        type="number"
-                        readOnly
-                        className="form-input w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0 mt-2"
-                        placeholder=""
-                        id="cvv"
-                        name="number"
-                        required=""
-                      />
-                    </div>
-                  </div>
-                </form>
-
-                <div className="mt-4">
-                  <input
-                    type="submit"
-                    className="py-2 px-5 inline-block font-semibold tracking-wide border align-middle duration-500 text-base text-center bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md w-full"
-                    value="Continue to checkout"
-                  />
-                </div>
+                <h3 className="text-xl leading-normal font-semibold mt-6">
+                  BTC Exchange Fee $0.98
+                </h3>
               </div>
             </div>
 
@@ -192,85 +86,45 @@ function SingleOrderCheckout() {
               <div className="p-6 rounded-md shadow dark:shadow-gray-800">
                 <div className="flex justify-between items-center">
                   <h5 className="text-lg font-semibold">Pay with Bitcoin</h5>
-
-                  <Link className="bg-indigo-600 flex justify-center items-center text-white text-[10px] font-bold px-2.5 py-0.5 rounded-full h-5">
-                    3
-                  </Link>
-                </div>
+                   </div>
 
                 <div className="mt-4 rounded-md shadow dark:shadow-gray-800">
                   <div className="p-3 flex justify-between items-center">
                     <div>
-                      <h5 className="font-semibold">Product Name</h5>
-                      <p className="text-sm text-slate-400">
-                        Brief description
-                      </p>
+                      <h3>bar code here</h3>
                     </div>
-
-                    <p className="text-slate-400 font-semibold">$ 12</p>
                   </div>
                   <div className="p-3 flex justify-between items-center border border-gray-100 dark:border-gray-800">
                     <div>
-                      <h5 className="font-semibold">Second product</h5>
+                      <h5 className="font-semibold">Payment Detail</h5>
                       <p className="text-sm text-slate-400">
                         Brief description
                       </p>
                     </div>
-
-                    <p className="text-slate-400 font-semibold">$ 18</p>
-                  </div>
+                    </div>
                   <div className="p-3 flex justify-between items-center border border-gray-100 dark:border-gray-800">
                     <div>
-                      <h5 className="font-semibold">Third item</h5>
+                      <h5 className="font-semibold">Payment Unique Address</h5>
                       <p className="text-sm text-slate-400">
                         Brief description
                       </p>
-                    </div>
-
-                    <p className="text-slate-400 font-semibold">$ 20</p>
-                  </div>
+                    </div> 
+                   </div>
                   <div className="p-3 flex justify-between items-center border border-gray-100 dark:border-gray-800 bg-gray-50 dark:bg-slate-800 text-green-600">
                     <div>
-                      <h5 className="font-semibold">Promo code</h5>
-                      <p className="text-sm text-green-600">EXAMPLECODE</p>
+                      <h5 className="font-semibold">Amount TO Be Pay</h5>
+                      <p className="text-sm text-green-600"></p>
                     </div>
-
-                    <p className="text-red-600 font-semibold">-$ 10</p>
-                  </div>
-                  <div className="p-3 flex justify-between items-center border border-gray-100 dark:border-gray-800">
-                    <div>
-                      <h5 className="font-semibold">Total (USD)</h5>
-                    </div>
-
-                    <p className="font-semibold">$ 30</p>
-                  </div>
-                </div>
-
-                <div className="subcribe-form mt-6">
-                  <form className="relative max-w-xl">
-                    <input
-                      type="email"
-                      id="subcribe"
-                      name="email"
-                      className="pt-4 pe-40 pb-4 ps-6 w-full h-[50px] outline-none text-black dark:text-white rounded-full bg-white dark:bg-slate-900 shadow dark:shadow-gray-800"
-                      placeholder="Promo code"
-                    />
-                    <button
-                      type="submit"
-                      className="py-2 px-5 inline-block font-semibold tracking-wide align-middle transition duration-500 ease-in-out text-base text-center absolute top-[2px] end-[3px] h-[46px] bg-indigo-600 hover:bg-indigo-700 border border-indigo-600 hover:border-indigo-700 text-white rounded-full"
-                    >
-                      Redeem
-                    </button>
-                  </form>
-                </div>
-              </div>
+                   </div>
+               </div>
             </div>
           </div>
         </div>
+       </div>
 
-        {/* <MobileApp/> */}
+      
       </section>
-      {/* <ShopFooter/> */}
+    
     </>
   );
 }
