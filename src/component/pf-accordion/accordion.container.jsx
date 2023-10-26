de8ae9908ea73a2ca02f94f3eb73a2012b331996
Md.Mohiuddin <saddammd203@gmail.com>
2023-10-27 03:10:27 +0600

    resolved

--- src/component/pf-accordion/accordion.container.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import SingleAccordion from "./single-accordion";
-
-const PFAccordion = ({ data }) => {
-  return data?.map((item) => <SingleAccordion key={item?.key} item={item} />);
-};
-
-export { PFAccordion };
