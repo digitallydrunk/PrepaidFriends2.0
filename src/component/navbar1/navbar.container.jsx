3c14c2e0744d5958f91e06131ccc3c592ad9dc55
rituraj singh <riturajsingh5454@gmail.com>
2023-10-27 22:37:05 +0530

    fix hambergerfix problem

--- src/component/navbar1/navbar.container.jsx
+++ src/component/navbar1/navbar.container.jsx
@@ -142,7 +142,7 @@ const Navbar = () => {
 
         <div className="menu-extras">
           <div className="menu-item">
-            <Link
+            <span
               className={`navbar-toggle ${isMenu ? "open" : ""}`}
               id="isToggle"
               onClick={() => toggleMenu()}
@@ -152,7 +152,7 @@ const Navbar = () => {
                 <span></span>
                 <span></span>
               </div>
-            </Link>
+            </span>
           </div>
         </div>
 
