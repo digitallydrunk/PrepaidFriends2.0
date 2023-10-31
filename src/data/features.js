fdf2f610022fb0fb7d36a5b6bb049f73ac7b05b1
Vedansh Mehra <vedanshofficial@gmail.com>
2023-11-01 02:44:59 +0530

    added our features section on the homepage

---
+++ src/data/features.js
@@ -0,0 +1,20 @@
+import { AiOutlineFire } from "react-icons/ai";
+import { MdOutlineJoinInner, MdOutlineSelectAll } from "react-icons/md";
+
+export const featuresData = [
+  {
+    icon: MdOutlineJoinInner,
+    title: "Automatic Payouts",
+    desc: "If the distribution of letters and words is random, the reader will not be distracted from making.",
+  },
+  {
+    icon: MdOutlineSelectAll,
+    title: "Incoming Payments",
+    desc: "If the distribution of letters and words is random, the reader will not be distracted from making.",
+  },
+  {
+    icon: AiOutlineFire,
+    title: "Cash Management",
+    desc: "If the distribution of letters and words is random, the reader will not be distracted from making.",
+  },
+];
--- src/pages/pf-home/homepage.container.jsx
+++ src/pages/pf-home/homepage.container.jsx
@@ -1,10 +1,59 @@
 import { faqData } from "../../data/faq";
 import PFAccordion from "../../component/pf-accordion";
+import { featuresData } from "../../data/features";
+import * as Icon from "react-feather";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <>
-      <div className="container relative md:mt-24 mt-16">
+      <section className="relative md:py-24 py-16 bg-gray-50 dark:bg-slate-800">
+        <div className="container relative">
+          <div className="grid grid-cols-1 pb-8 text-center">
+            <h3 className="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">
+              Our Features
+            </h3>
+
+            <p className="text-slate-400 max-w-xl mx-auto">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+            </p>
+          </div>
+
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-4 gap-[30px]">
+            {featuresData.map((item, index) => {
+              let Icons = item.icon;
+              return (
+                <div
+                  key={index}
+                  className="group relative lg:px-6 mt-4 transition duration-500 ease-in-out rounded-xl overflow-hidden text-center"
+                >
+                  <div className="relative overflow-hidden text-transparent -m-3">
+                    <Icon.Hexagon className="h-28 w-28 fill-indigo-600/5 mx-auto rotate-[30deg]"></Icon.Hexagon>
+                    <div className="absolute top-2/4 -translate-y-2/4 start-0 end-0 mx-auto text-indigo-600 rounded-xl transition duration-500 ease-in-out text-3xl flex align-middle justify-center items-center">
+                      <Icons />
+                    </div>
+                  </div>
+
+                  <div className="mt-6">
+                    <Link
+                      href="#"
+                      className="text-xl font-medium hover:text-indigo-600 duration-500 ease-in-out"
+                    >
+                      {item.title}
+                    </Link>
+                    <p className="text-slate-400 transition duration-500 ease-in-out mt-3">
+                      {item.desc}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      <div className="container relative mt-16">
         <div className="grid grid-cols-1 pb-8 text-center">
           <h3 className="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">
             Frequently Asked Questions
