2959d328d7407a50c6e7cfd4b3094faaf0285166
iamavs09 <aryanvikas009@gmail.com>
2023-11-01 03:16:19 +0530

    fixed overlapping

--- src/component/pf-accordion/single-accordion.jsx
+++ src/component/pf-accordion/single-accordion.jsx
@@ -1,27 +1,17 @@
 import React, { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
 import styles from "./accordion.module.css";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 
 const SingleAccordion = ({ item }) => {
   const [toggle, setToggle] = useState(false);
 
-  const accordionVariants = {
-    open: { height: 60 },
-    closed: { height: 0 },
-  };
-
   return (
-    <main>
-      <motion.div
+    <div className="shadow dark:shadow-gray-800 rounded-md overflow-hidden py-4 px-2 mt-4">
+      <div
         onClick={() => setToggle(!toggle)}
-        className="flex items-center justify-between py-8 gap-4 cursor-pointer"
-        initial={false}
-        animate={toggle ? "open" : "closed"}
-        variants={accordionVariants}
-        transition={{ type: "tween" }}
+        className={`flex items-center justify-between cursor-pointer `}
       >
-        <h4 className="font-bold text-lg">{item.title}</h4>
+        <h4 className={`font-bold text-lg ${toggle?styles["up-arrow"]:" "}`}>{item.title}</h4>
         <button>
           {toggle ? (
             <FiChevronUp className={`${styles["up-arrow"]} ${styles.bold}`} />
@@ -29,21 +19,13 @@ const SingleAccordion = ({ item }) => {
             <FiChevronDown className={styles.bold} />
           )}
         </button>
-      </motion.div>
-      <AnimatePresence>
-        {toggle && (
-          <motion.p
-            initial={{ opacity: 0, height: 0 }}
-            animate={{ opacity: 1, height: 30 }}
-            exit={{ opacity: 0, height: 0 }}
-            transition={{ duration: 0.1 }}
-            className="mb-4"
-          >
-            {item.content}
-          </motion.p>
-        )}
-      </AnimatePresence>
-    </main>
+      </div>
+      {toggle && (
+        <p className={styles.text}>
+          {item.content}
+        </p>
+      )}
+    </div>
   );
 };
 
