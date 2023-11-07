218e2448a0bb99c283ea8dbae3c19983b9f20ed6
Vedansh Mehra <vedanshofficial@gmail.com>
2023-11-08 02:15:41 +0530

    created custom hook for axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
+import dotenv from "dotenv";
+
+dotenv?.config();
 
 export default function App() {
   return <RouterProvider router={router} />;
