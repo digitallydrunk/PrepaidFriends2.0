3317a8f0ed717a5431e5fa2b713413fc4dbdc767
shreya072001 <gsmile.india201@gmail.com>
2023-11-29 22:58:53 +0530

    added storage

--- src/data/home-banner.js
+++ src/data/home-banner.js
@@ -2,23 +2,23 @@ const bannerData = [
   {
     id: 1,
     image:
-      "https://images.unsplash.com/photo-1449034446853-66c86144b0ad?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    heading: "Welcome to Prepaid Friends",
-    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit Doloribus dicta magnam",
+      "https://st2.depositphotos.com/1743476/45995/i/450/depositphotos_459958508-stock-photo-close-young-woman-hand-holding.jpg",
+    heading: "Solo Solutions for Every Purchase",
+    info: "Secure Your Transactions with Our Individual Prepaid Cards",
   },
   {
     id: 2,
     image:
-      "https://images.unsplash.com/photo-1449034446853-66c86144b0ad?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    heading: "This is heading",
-    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit Doloribus dicta magnam",
+    "https://image.cnbcfm.com/api/v1/image/106687880-1599052381377-gettyimages-1172101452-dscf4217.jpeg?v=1604332646&w=720&h=405",
+    heading: "Streamlined Solutions for Multiple Transactions",
+    info: "Effortless Management for Your Multi-Card Purchase Needs",
   },
   {
     id: 3,
     image:
-      "https://images.unsplash.com/photo-1449034446853-66c86144b0ad?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    heading: "This is heading",
-    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit Doloribus dicta magnam",
+      "https://www.axisbank.com/images/default-source/revamp_new/cards/prepaid-cards/desktop/pre-paid-cards.jpg?sfvrsn=d0758e55_2",
+    heading: "Brokers",
+    info: "",
   },
 ];
 
